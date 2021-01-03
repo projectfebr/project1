7cdce9c3e129e2cadc5b9c56be4b56ebecce9f5b
Bolotov Alexandr <projectfebr@gmail.com>
2021-01-03 16:35:43 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.scss';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { UsersPage } from './components/UsersPage/UsersPage';
+import { ServicesPage } from './components/ServicesPage/ServicesPage';
+
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" render={props => <UsersPage/>}/>
+          <Route exact path="/services" render={props => <ServicesPage/>}/>
+        </Switch>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ServicesPage/ServicesPage.js
@@ -0,0 +1,12 @@
+import { CButton, CHeaderNav, CHeaderNavItem } from '@coreui/react';
+import React from 'react';
+import './ServicesPage.css'
+
+
+
+export function ServicesPage() {
+    return (
+        <div>ServicesPage</div>
+    )
+
+};
